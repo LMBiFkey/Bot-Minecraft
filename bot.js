0e2cf04b89178bf2a3913874681c6a730e943af1
LMBiFkey <42891784+LMBiFkey@users.noreply.github.com>
2018-09-01 15:17:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
  });
  
  // THIS MUST BE THIS WAY
- client.login(process.env.BOT_TOKEN);
+ client.login(process.env.NDg1NDIwODEyNTI3MDc1MzI4.DmwTiA.Bu1efaWE0ddN8srgIKBvgOg-wKw);
